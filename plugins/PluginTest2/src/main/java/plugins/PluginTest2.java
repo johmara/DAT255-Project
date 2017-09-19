eb53db387bc3b1991635a9a0178dd21a118ecfcb
Joakim willard <joakimwillard@yahoo.se>
2017-09-19 17:30:50 +0200

    Plugin that prints wheelspeed

---
+++ plugins/PluginTest2/src/main/java/plugins/PluginTest2.java
@@ -0,0 +1,57 @@
+package plugins;
+
+import com.sun.squawk.VM;
+import sics.plugin.PlugInComponent;
+import sics.port.PluginPPort;
+import sics.port.PluginRPort;
+
+public class PluginTest2 extends PlugInComponent {
+
+    public PluginPPort speed;
+    public PluginPPort steering;
+    public PluginRPort sensor;
+
+    public PluginTest2(String[] args) {
+        super(args);
+    }
+
+    public PluginTest2() {
+    }
+
+    public static void main(String[] args) {
+        VM.println("Circle.main()\r\n");
+        PluginTest2 ap = new PluginTest2(args);
+        ap.run();
+        VM.println("Circle-main done");
+    }
+
+    public void init() {
+        // Initiate PluginPPort
+        VM.println("init 1");
+        speed = new PluginPPort(this, "sp");
+        VM.println("init 2");
+        sensor = new PluginRPort(this, "se");
+        VM.println("init 3");
+        steering = new PluginPPort(this, "st");
+    }
+
+    public void doFunction() {
+        while (true) {
+            try {
+                steering.write(100);
+                Thread.sleep(2000);
+                speed.write(1);
+                Thread.sleep(2000);
+                speed.write(0);
+                VM.println(String.valueOf(sensor.readInt()));
+            } catch (InterruptedException e) {
+                VM.println("Interrupted.");
+            }
+        }
+    }
+
+    public void run() {
+        init();
+        doFunction();
+    }
+}
