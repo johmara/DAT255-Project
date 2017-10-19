3f3cf3794c4acbe7faf13d609610621d3331e9e2
Joakim willard <joakimwillard@yahoo.se>
2017-10-19 08:48:12 +0200

    Latest values for the regulator.

--- JavaInterfaceTest/src/absolut/acc/Regulator.java
+++ JavaInterfaceTest/src/absolut/acc/Regulator.java
@@ -18,9 +18,9 @@ public class Regulator {
     private void init(){
         preferredDistance = 40;
         kp = 0.85;
-        ki = 0.001;
+        ki = 0.00001;
         kd = 0.001;
-        dt = 100;
+        dt = 0.1;
         integral = 0;
         lastError = 0;
 
@@ -37,16 +37,16 @@ public class Regulator {
         double derivate = 0;
 
         integral = integral + (error * dt);
-        derivate = (error - lastError) / dt;
+        //derivate = (error - lastError) / dt;
 
-        integral = integral > 10 ? 10 : integral;
-        integral = integral < -4 ? -4 : integral;
+        integral = integral > 5 ? 5 : integral;
+        integral = integral < -10 ? -10 : integral;
 
         System.out.println("Intergral: " + integral);
 
-        controlSignal = kp * error + (ki * integral) + (kd * derivate);
+        controlSignal = kp * error + (ki * integral) + (kd * (error - lastError)/*derivate*/);
 
-        controlSignal = clamp(Math.round(controlSignal), 0, 100);
+        controlSignal = clamp(Math.round(controlSignal), -100, 100);
 
         if(controlSignal > 40){
             controlSignal = 40;
@@ -58,11 +58,11 @@ public class Regulator {
         System.out.println(controlSignal);
 
         lastError = error;
-        try {
+        /*try {
             Thread.sleep((long)dt);
         } catch (InterruptedException e) {
             e.printStackTrace();
-        }
+        }*/
         return (int) Math.round(controlSignal);
     }
 
